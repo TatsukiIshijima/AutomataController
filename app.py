b4e6175549af6ea4150bb8a16c57c260c9525644
TatsukiIshijima <tatsuki445@gmail.com>
2020-01-19 18:34:59 +0900

    Flaskでの制御画面作成

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def form():
+	return redirect(url_for('home'))
+
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	if request.method == 'POST':
+		if request.form.get('Forward') == 'Forward':
+			print('Forward')
+		elif request.form.get('Backward') == 'Backward':
+			print('Backward')
+		elif request.form.get('Left') == 'Left':
+			print('Lef')
+		elif request.form.get('Right') == 'Right':
+			print('Right')
+		elif request.form.get('Stop') == 'Stop':
+			print('Stop')
+	return render_template('home.html')
+
+
+'''
+def __update_drive_error(response):
+	if response is not None:
+		flash(f'{response.message}')
+'''
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host='0.0.0.0', port=80)
