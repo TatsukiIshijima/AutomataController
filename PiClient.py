0f1354f57c05b2fbe39ae10f2c49aa4d857ab4d9
TatsukiIshijima <tatsuki445@gmail.com>
2020-03-12 15:53:45 +0900

    制御系のメソッド追加

--- PiClient.py
+++ PiClient.py
@@ -2,12 +2,62 @@ import sys
 import time
 
 import serial
+from serial.tools import list_ports
 
 LED_COMMAND = "led"
 DIRECTION_SERVO_COMMAND = "d_servo_"
 FORWARD_MOTOR_COMMAND = "f_motor_"
 BACKWARD_MOTOR_COMMAND = "b_motor_"
 STOP_MOTOR_COMMAND = "s_motor"
+
+
+class PiClient:
+
+	def __init__(self):
+		self._ser = serial.Serial()
+		self._ser.baudrate = 9600
+		self._ser.timeout = 1
+		self._ser.write_timeout = 1
+		self._ports = list_ports.comports()
+		self._devices = [info.device for info in self._ports]
+		if len(self._devices) != 1:
+			print('error : device not found.')
+			sys.exit()
+		print("device found %s" % self._devices[0])
+		self._ser.port = self._devices[0]
+		try:
+			self._ser.open()
+			time.sleep(2)
+		except:
+			print('error : can not open serial.')
+			sys.exit()
+
+	def _write(self, command_str):
+		if self._ser.is_open:
+			self._ser.write(command_str.encode('utf-8'))
+
+	def launch_led(self):
+		self._write(LED_COMMAND + ';')
+
+	def accelerate(self, power, is_forward=True):
+		if power < 0 or power > 255:
+			return
+		elif power == 0:
+			self._write(STOP_MOTOR_COMMAND + ';')
+		else:
+			if is_forward:
+				self._write(FORWARD_MOTOR_COMMAND + str(power) + ';')
+			else:
+				self._write(BACKWARD_MOTOR_COMMAND + str(power) + ';')
+
+	def turn(self, direction):
+		if direction < 0 or direction > 180:
+			return
+		self._write(DIRECTION_SERVO_COMMAND + str(direction) + ';')
+
+	def close(self):
+		self._ser.close()
+
 
 if __name__ == '__main__':
 
