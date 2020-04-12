9c2d576c3dba4b998b8bcc346567b1c370b6d1b7
ishijimaitsuki <tatsuki445@gmail.com>
2020-04-12 13:44:23 +0900

    受信メソッドのリネーム

--- server.py
+++ server.py
@@ -3,20 +3,16 @@ import ipget
 import websockets
 
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
-
-    greeting = f"Hello {name}!"
-
-    await websocket.send(greeting)
-    print(f" >{greeting}")
+async def __receive_command(websocket, path):
+    command = await websocket.recv()
+    if command is not None:
+        print(f"> receive {command}")
 
 if __name__ == '__main__':
 
     try:
         ip = ipget.ipget().ipaddr("wlan0")
-        start_server = websockets.serve(hello, f"{ip.split('/')[0]}", 8765)
+        start_server = websockets.serve(__receive_command, f"{ip.split('/')[0]}", 8765)
         print(f"launch web socket server {ip.split('/')[0]}:8765")
 
         asyncio.get_event_loop().run_until_complete(start_server)
